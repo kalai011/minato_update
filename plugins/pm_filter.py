940f98352e351224f35a3f6c28eb2d4b3c29d253
kalai011 <147304338+kalai011@users.noreply.github.com>
2023-10-26 17:50:38 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1289,13 +1289,13 @@ async def cb_handler(client: Client, query: CallbackQuery):
         buttons = [[
                     InlineKeyboardButton('⤬ Aᴅᴅ Mᴇ Tᴏ Yᴏᴜʀ Gʀᴏᴜᴘ ⤬', url=f'http://telegram.me/{temp.U_NAME}?startgroup=true')
                 ],[
-                    InlineKeyboardButton('ᴀᴅꜱ ꜰʀᴇᴇ ᴍᴏᴠɪᴇ 🎥', callback_data="shortlink_info"),
+                    InlineKeyboardButton('💰 ᴅᴏɴᴀᴛᴇ 💰', callback_data="shortlink_info"),
                     InlineKeyboardButton('⌬ Mᴏᴠɪᴇ Gʀᴏᴜᴘ', url=GRP_LNK)
                 ],[
                     InlineKeyboardButton('〄 Hᴇʟᴘ', callback_data='help'),
                     InlineKeyboardButton('⍟ Aʙᴏᴜᴛ', callback_data='about')
                 ],[
-                  InlineKeyboardButton('💰 ᴅᴏɴᴀᴛᴇ 💰', callback_data='source')
+                  InlineKeyboardButton('ᴀᴅꜱ ꜰʀᴇᴇ ᴍᴏᴠɪᴇ 🎥', callback_data='source')
                 ],[
                     InlineKeyboardButton('✇ Jᴏɪɴ Uᴘᴅᴀᴛᴇs Cʜᴀɴɴᴇʟ ✇', url=CHNL_LNK)
                   ]]
@@ -1384,7 +1384,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
     elif query.data == "about":
         buttons = [[
             InlineKeyboardButton('Sᴜᴘᴘᴏʀᴛ Gʀᴏᴜᴘ', url=GRP_LNK),
-            InlineKeyboardButton('💰ᴅᴏɴᴀᴛᴇ💰', callback_data='source')
+            InlineKeyboardButton('ᴀᴅꜱ ꜰʀᴇᴇ ᴍᴏᴠɪᴇ 🎥', callback_data='source')
         ],[
             InlineKeyboardButton('Hᴏᴍᴇ', callback_data='start'),
             InlineKeyboardButton('Cʟᴏsᴇ', callback_data='close_data')
