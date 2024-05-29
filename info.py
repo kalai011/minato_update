9c4f2ca22436545a3ecf627d9821d6f4560b4540
kalai011 <147304338+kalai011@users.noreply.github.com>
2024-05-29 15:33:54 +0530

    Update info.py

--- info.py
+++ info.py
@@ -50,9 +50,9 @@ COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Others
 VERIFY = bool(environ.get('VERIFY', False))
-SHORTLINK_URL = environ.get('SHORTLINK_URL', 'Kingurl.in')
-SHORTLINK_API = environ.get('SHORTLINK_API', '983696e85ad30224b3d419b6cde922a6df998ed1')
-IS_SHORTLINK = bool(environ.get('IS_SHORTLINK', False))
+SHORTLINK_URL = environ.get('SHORTLINK_URL', 'gplinks.com')
+SHORTLINK_API = environ.get('SHORTLINK_API', 'd8a56d59602cfaba6d015be7a8482234b5622b2f')
+IS_SHORTLINK = bool(environ.get('IS_SHORTLINK',True))
 DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in environ.get('DELETE_CHANNELS', '0').split()]
 MAX_B_TN = environ.get("MAX_B_TN", "5")
 MAX_BTN = is_enabled((environ.get('MAX_BTN', "True")), True)
